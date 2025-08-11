8e47a8e30be47ef92b757bea38edd0598be3ab6f
Uday K <asonduk.1@gmail.comm>
2025-08-11 23:08:07 +0530

    first commit

---
+++ main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	jainIntro := "welcome jain to swish"
+	introTwo := "welcome sanjay to swish"
+	introThree := "welcome pardhu to swish"
+
+	finalize := []string{jainIntro, introTwo, introThree}
+
+	order()
+	totaluser(finalize)
+}
+
+func totaluser(finalize []string) {
+	for index, final := range finalize {
+		fmt.Printf("%d. %s \n", index+1, final)
+	}
+}
+
+func order() {
+	var cName = "SWISH"
+	var items1 = "samosa"
+	var item2 = "biryani"
+	var item3 = "chicken curry"
+	var item4 = "chocolate cake"
+	var item5 = "ice cream"
+	var contact = "justswish@gmail.com"
+	fmt.Println("WELCOME TO", cName)
+	fmt.Println("Please enter your name")
+
+	var userName string
+	var itemsneed string
+	var paymentOption string
+	var address string
+
+	fmt.Scan(&userName)
+	// fmt.Printf("Hello %v, please select the item you want to order \n", userName)
+	fmt.Printf("Hello %v, Please select an item from the menu %v %v %v %v %v for more details contact %v\n", userName, items1, item2, item3, item4, item5, contact)
+
+	fmt.Scan(&itemsneed)
+	fmt.Printf("You have selected %v, please select your payment option\n", itemsneed)
+
+	fmt.Scan(&paymentOption)
+	fmt.Printf("You have selected %v as your payment option, please enter your address\n", paymentOption)
+
+	fmt.Scan(&address)
+	fmt.Printf("Your order for %v has been placed successfully. It will be delivered to %v. Thank you for choosing %v!\n", itemsneed, address, cName)
+	fmt.Println("For any queries, please contact us at", contact)
+
+}
